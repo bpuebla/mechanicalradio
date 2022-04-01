0bfecab78fef6f4e32b3c56a7298b0c801786e58
Bautista <0bautistapuebla@gmail.com>
2022-04-01 10:43:00 +0200

    implemented form

--- lib/main.dart
+++ lib/main.dart
@@ -90,17 +90,20 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            ElevatedButton(
-                onPressed: _togglePlaying,
-                child: (_isPlaying
-                    ? const Icon(Icons.pause)
-                    : const Icon(Icons.play_arrow))),
-            Text(
-              'Press Play',
-              style: Theme.of(context).textTheme.headline5,
-            ),
+            const MyCustomForm(),
+            Column(children: [
+              ElevatedButton(
+                  onPressed: _togglePlaying,
+                  child: (_isPlaying
+                      ? const Icon(Icons.pause)
+                      : const Icon(Icons.play_arrow))),
+              Text(
+                'Press Play',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ]),
           ],
         ),
       ),
@@ -114,6 +117,64 @@ class _MyHomePageState extends State<MyHomePage> {
           label: 'Info',
         ),
       ]),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
